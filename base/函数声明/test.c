5803c34463189cd19ad1110c183cc738e00648ed
AzeHan <32954669+HananxR@users.noreply.github.com>
2023-01-06 16:30:26 +0800

    函数声明、实现、引用

---
+++ base/函数声明/test.c
@@ -0,0 +1,6 @@
+#include "test.h"
+
+//函数Add的实现
+int Add(int x, int y) {
+	return x + y;
+}
---
+++ base/函数声明/test.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef __TEST_H__
+#define __TEST_H__
+//函数的声明
+int Add(int x, int y);
+#endif //__TEST_H__
+
